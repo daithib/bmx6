0b18ac6cf7d111608ee90368a612a8a34c0e363b
Axel Neumann <neumann@cgws.de>
2013-06-30 14:25:58 +0200

    sms: use tx_iterator_cache_msg_space_pref() instead of .._space_max() for creation of desc extensions

--- lib/bmx6_sms/sms.c
+++ lib/bmx6_sms/sms.c
@@ -212,7 +212,7 @@ int create_description_sms(struct tx_frame_iterator *it)
         struct json_sms *sms;
 
         uint8_t *data = tx_iterator_cache_msg_ptr(it);
-        uint16_t max_size = tx_iterator_cache_data_space_max(it);
+        uint16_t max_size = tx_iterator_cache_data_space_pref(it);
         int pos = 0;
 
         while ((sms = avl_iterate_item(&json_sms_tree, &an))) {
